367e0dd78e7de032c8c19278eb7caec7e7a7c92e
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-26 16:43:30 +0530

    Update Proj.cpp

--- Proj.cpp
+++ Proj.cpp
@@ -70,22 +70,20 @@ void start_screen(int i,int j)
 
 void sun()
 {
-
- float theta;
-
-GLfloat angle;
-glLineWidth(1.5);
-glColor3f(1,0.2,0);
-glBegin(GL_POLYGON);
-for (int i = 0; i <360 ; i++)
-{
-theta  = i*PI*i/180;
-glVertex2f(500+80*cos(theta)/2, 600+80*sin(theta)/2);
+   float theta;
+   GLfloat angle;
+   glLineWidth(1.5);
+   glColor3f(1,0.2,0);
+   glBegin(GL_POLYGON);
+   for (int i = 0; i <360 ; i++)
+   {
+      theta  = i*PI*i/180;
+      glVertex2f(500+80*cos(theta)/2, 600+80*sin(theta)/2);
+   }
+   glEnd();
+   glFlush();
 }
 
-glEnd();
-glFlush();
-}
 void cloud1()
 {
 
